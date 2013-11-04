25eaf29c583f584eff1d729d3b39033f69145919
epifanio <epiesasha@me.com>
2013-11-03 19:42:59 -0500

    update keys on prov dictionary (draft)

--- ecooprovdict.py
+++ ecooprovdict.py
@@ -1,7 +1,46 @@
 # coding: utf-8
 ecooProvDict = {}
+
+ecooProvDict['IPythonNotebook'] = {}
+ecooProvDict['IPythonNotebook']['title'] = ''
+ecooProvDict['IPythonNotebook']['description'] = ''
+ecooProvDict['IPythonNotebook']['accessURL'] = ''
+
+
 ecooProvDict['IPythonNotebookRun'] = {}
 ecooProvDict['IPythonNotebookRun']['description'] = ''
 ecooProvDict['IPythonNotebookRun']['startedAtTime'] = ''
 ecooProvDict['IPythonNotebookRun']['endedAtTime'] = ''
 ecooProvDict['IPythonNotebookRun']['wasAssociatedWith'] = ''
+
+
+ecooProvDict['CellRun'] = {}
+ecooProvDict['CellRun']['description'] = ''
+ecooProvDict['CellRun']['startedAtTime'] = ''
+ecooProvDict['CellRun']['endedAtTime'] = ''
+ecooProvDict['CellRun']['wasAssociatedWith'] = ''
+
+
+ecooProvDict['Document'] = {}
+ecooProvDict['Document']['title'] = ''
+ecooProvDict['Document']['description'] = ''
+ecooProvDict['Document']['downloadURL'] = ''
+
+
+ecooProvDict['Dataset'] = {}
+ecooProvDict['Dataset']['title'] = ''
+ecooProvDict['Dataset']['description'] = ''
+ecooProvDict['Dataset']['distribution'] = {}
+ecooProvDict['Dataset']['distribution']['title'] = ''
+ecooProvDict['Dataset']['distribution']['description'] = ''
+ecooProvDict['Dataset']['distribution']['accessURL'] = ''
+ecooProvDict['Dataset']['distribution']['downloadURL'] = ''
+ecooProvDict['Dataset']['wasAttributedTo'] = ''
+
+
+ecooProvDict['Library'] = {}
+ecooProvDict['Library']['title'] = ''
+ecooProvDict['Library']['description'] = ''
+ecooProvDict['Library']['accessURL'] = ''
+ecooProvDict['Library']['wasAttributedTo'] = ''
+
