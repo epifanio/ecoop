fb45abb4c9beb9892ab226e5c55c8133cb2825d3
epifanio <epiesasha@me.com>
2013-11-20 20:20:54 -0500

    work on centos installation

--- ecoop_install/yuminstall.sh
+++ ecoop_install/yuminstall.sh
@@ -1,9 +1,6 @@
 yum -y install gcc gcc-c++ gcc-gfortran blas-devel lapack-devel zlib-devel bzip2-devel openssl-devel ncurses-devel sqlite-devel readline-devel tk-devel \
 freetype-devel libpng-devel svn git python-devel ruby texinfo texinfo-tex \
 libxml2-devel libcurl-devel libtiff-devel mesa-libGLU-devel mesa-libGLw-devel cairo-devel pcre-devel ImageMagick-devel \
-ImageMagick-c++-devel gnuplot-latex gsl-devel gtk2-devel java-1.7.0-openjdk-devel fftw-devel rubygems ruby-devel  >> yuminstall_devlibs.log
-yum -y groupinstall "Development tools" >> yuminstall_devtools.log
-gem install jist jist >> yuminstall_gems.log
-
-
-
+ImageMagick-c++-devel gnuplot-latex gsl-devel gtk2-devel java-1.7.0-openjdk-devel fftw-devel rubygems ruby-devel  
+yum -y groupinstall "Development tools" 
+gem install jist 
