243c1c690d1466c9452541ac49d0b01a88ece4ab
epifanio <epiesasha@me.com>
2014-02-02 18:02:48 -0500

    added numba and dependecies

---
+++ ecoop_install/install_ld.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+###############################################################################
+#
+#
+# Project: ECOOP, sponsored by The National Science Foundation
+# Purpose: this code is part of the Cyberinfrastructure developed for the ECOOP project
+#                http://tw.rpi.edu/web/project/ECOOP
+#                from the TWC - Tetherless World Constellation
+#                            at RPI - Rensselaer Polytechnic Institute
+#                            founded by NSF
+#
+# Author:   Massimo Di Stefano , distem@rpi.edu -
+#                http://tw.rpi.edu/web/person/MassimoDiStefano
+#
+###############################################################################
+# Copyright (c) 2008-2014 Tetherless World Constellation at Rensselaer Polytechnic Institute
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+# OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+# DEALINGS IN THE SOFTWARE.
+###############################################################################
+
+﻿np=`cat /proc/cpuinfo | grep processor | wc -l`
+
+# PYTHON
+
+CURRENTDIR=${PWD}
+BUILD=epilib
+PREFIX=/home/$USER/Envs/env1
+
+TEMPBUILD=/home/$USER/$BUILD
+mkdir -p $TEMPBUILD
+mkdir -p $TEMPBUILD/tarball
+mkdir -p $TEMPBUILD/src
+
+cd $TEMPBUILD
+export PATH=$PREFIX/bin:$PATH
+export LD_LIBRARY_PATH=$PREFIX/lib:$PREFIX/lib64:$LD_LIBRARY_PATH
+
+### json-ld
+
+git clone https://github.com/digitalbazaar/pyld.git
+cd pyld
+$PREFIX/bin/python setup.py install >> ../pyinstall.log
+rm -rf build
+cd $TEMPBUILD
+mv pyld $TEMPBUILD/src
+
+echo "installing rdflib"
+$PREFIX/bin/pip install rdflib >> pip.log
+
+git clone https://github.com/RDFLib/rdflib-jsonld.git
+cd rdflib-jsonld
+$PREFIX/bin/python setup.py install >> ../pyinstall.log
+rm -rf build
+cd $TEMPBUILD
+mv rdflib-jsonld $TEMPBUILD/src
