450dfbec6fba5cc2667f3fe068bc8d8c02032c87
epifanio <epiesasha@me.com>
2014-02-03 12:26:54 -0500

    removed imagemagix (broken package)

--- ecoop_install/apt-get_install.sh
+++ ecoop_install/apt-get_install.sh
@@ -39,9 +39,10 @@ sudo apt-get install -y build-essential liblapack-dev libzzip-dev libssl-dev \
 libncurses5-dev libsqlite3-dev libreadline-dev tk-dev tcl-dev \
 libfreetype6-dev libpng12-dev subversion git libxml2-dev libcurl4-gnutls-dev \
 libtiff5-dev libglu1-mesa-dev libglw1-mesa-dev \
-libcairo2-dev libpcre3-dev libmagick++-dev gnuplot-x11 texlive libgsl0-dev \
-libgtk2.0-dev openjdk-7-jre openjdk-7-jdk libfftw3-dev libssl-dev libreadline-dev >> apt-get.log
+libcairo2-dev libpcre3-dev gnuplot-x11 texlive libgsl0-dev \
+libgtk2.0-dev openjdk-7-jre openjdk-7-jdk libfftw3-dev libssl-dev libreadline-dev pandoc cabal-install >> apt-get.log
 
+# texlive-latex-recommended libmagick++-dev
 sudo apt-get -y install autoconf automake build-essential git libass-dev libgpac-dev \
 libsdl1.2-dev libtheora-dev libtool libva-dev libvdpau-dev libvorbis-dev libx11-dev \
 libxext-dev libxfixes-dev pkg-config texi2html zlib1g-dev yasm libmp3lame-dev nasm libvpx-dev libxslt-dev flex bison libffi-dev
