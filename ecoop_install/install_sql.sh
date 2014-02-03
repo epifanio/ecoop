da42b2d25a3a24471b43a80f3f5c95fe96d3249c
epifanio <epiesasha@me.com>
2014-02-03 18:14:41 -0500

    removed log to file

--- ecoop_install/install_sql.sh
+++ ecoop_install/install_sql.sh
@@ -55,10 +55,10 @@ echo "installing freexl"
 wget http://www.gaia-gis.it/gaia-sins/freexl-sources/freexl-1.0.0g.tar.gz
 tar -zxf freexl-1.0.0g.tar.gz
 cd freexl-1.0.0g
-./configure --prefix=$PREFIX/ >> ../freexl_configure.log
-make -j $np >> ../freexl_build.log
-make install >> ../freexl_install.log
-make distclean > /dev/null 2>&1
+./configure --prefix=$PREFIX/
+make -j $np
+make install
+make distclean
 cd $TEMPBUILD
 mv freexl-1.0.0g.tar.gz $TEMPBUILD/tarball
 mv freexl-1.0.0g $TEMPBUILD/src
@@ -68,9 +68,9 @@ echo "installing libspatialite"
 wget http://www.gaia-gis.it/gaia-sins/libspatialite-sources/libspatialite-4.1.1.tar.gz
 tar -zxf libspatialite-4.1.1.tar.gz
 cd libspatialite-4.1.1
-CPPFLAGS=-I$PREFIX/include/ LDFLAGS=-L$PREFIX/lib ./configure --with-geosconfig=$PREFIX/bin/geos-config --prefix=$PREFIX/ >> ../libspatialite_configure.log
-make -j $np >> ../libspatialite_build.log
-make install >> ../libspatialite_install.log
+CPPFLAGS=-I$PREFIX/include/ LDFLAGS=-L$PREFIX/lib ./configure --with-geosconfig=$PREFIX/bin/geos-config --prefix=$PREFIX/
+make -j $np
+make install
 make distclean > /dev/null 2>&1
 cd $TEMPBUILD
 mv libspatialite-4.1.1.tar.gz $TEMPBUILD/tarball
@@ -81,9 +81,9 @@ echo "installing postgresql"
 wget http://ftp.postgresql.org/pub/source/v9.3.0/postgresql-9.3.0.tar.gz
 tar -zxf postgresql-9.3.0.tar.gz
 cd postgresql-9.3.0
-./configure --prefix=$PREFIX/ >> ../postgresql_configure.log
-make -j $np >> ../postgresql_build.log
-make install >> ../postgresql_install.log
+./configure --prefix=$PREFIX/
+make -j $np
+make install
 make distclean > /dev/null 2>&1
 cd $TEMPBUILD
 mv postgresql-9.3.0.tar.gz $TEMPBUILD/tarball
@@ -97,7 +97,7 @@ wget http://initd.org/psycopg/tarballs/PSYCOPG-2-5/psycopg2-2.5.1.tar.gz
 tar -zxf psycopg2-2.5.1.tar.gz
 cd psycopg2-2.5.1
 $PREFIX/bin/python setup.py build_ext --pg-config $PREFIX/bin/pg_config 
-$PREFIX/bin/python setup.py install >> ../pyinstall.log
+$PREFIX/bin/python setup.py install
 rm -rf build
 cd $TEMPBUILD 
 mv psycopg2-2.5.1.tar.gz $TEMPBUILD/tarball
