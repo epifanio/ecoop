caac9edb1efa6901d926967c1755f3b439fc3b93
epifanio <epiesasha@me.com>
2014-02-03 13:02:46 -0500

    comented broken package in apt-get

--- ecoop_install/apt-get_install.sh
+++ ecoop_install/apt-get_install.sh
@@ -42,7 +42,9 @@ libtiff5-dev libglu1-mesa-dev libglw1-mesa-dev \
 libcairo2-dev libpcre3-dev gnuplot-x11 texlive libgsl0-dev \
 libgtk2.0-dev openjdk-7-jre openjdk-7-jdk libfftw3-dev libssl-dev libreadline-dev pandoc cabal-install >> apt-get.log
 
-# texlive-latex-recommended libmagick++-dev
+# texlive-latex-recommended
+# libmagick++-dev  # broken
+
 sudo apt-get -y install autoconf automake build-essential git libass-dev libgpac-dev \
 libsdl1.2-dev libtheora-dev libtool libva-dev libvdpau-dev libvorbis-dev libx11-dev \
 libxext-dev libxfixes-dev pkg-config texi2html zlib1g-dev yasm libmp3lame-dev nasm libvpx-dev libxslt-dev flex bison libffi-dev
