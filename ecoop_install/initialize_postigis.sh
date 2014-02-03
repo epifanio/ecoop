fb8f121a8fdd41de61253006f83c76c9d7eb63c9
epifanio <epiesasha@me.com>
2014-02-03 13:02:21 -0500

    added postgis initialization

---
+++ ecoop_install/initialize_postigis.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+###############################################################################
+#
+#
+# Project: ECOOP, sponsored by The National Science Foundation
+# Purpose: this code is part of the Cyberinfrastructure developed for the ECOOP project
+#                http://tw.rpi.edu/web/project/ECOOP
+#                from the TWC - Tetherless World Constellation
+#                            at RPI - Rensselaer Polytechnic Institute
+#                            founded by NSF
+#
+# Author:   Massimo Di Stefano , distem@rpi.edu -
+#                http://tw.rpi.edu/web/person/MassimoDiStefano
+#
+###############################################################################
+# Copyright (c) 2008-2014 Tetherless World Constellation at Rensselaer Polytechnic Institute
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+# OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+# DEALINGS IN THE SOFTWARE.
+###############################################################################
+
+
+﻿np=`cat /proc/cpuinfo | grep processor | wc -l`
+
+PREFIX=/home/$USER/Envs/env1
+
+export PATH=$PREFIX/bin:$PATH
+export LD_LIBRARY_PATH=$PREFIX/lib:$PREFIX/lib64:$LD_LIBRARY_PATH
+
+$PREFIX/bin/createdb test
+$PREFIX/bin/psql -f $PREFIX/share/postgresql/contrib/postgis-2.1/postgis.sql -d test
+$PREFIX/bin/psql -f /home/$USER/Envs/env1/share/postgresql/contrib/postgis-2.1/spatial_ref_sys.sql -d test
